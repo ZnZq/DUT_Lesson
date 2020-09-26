623cdf9e2571462546ebc1220e011c5f489e2eb6
ZnZq <gazttgaz@gmail.com>
2020-09-26 22:36:36 +0300

    1

---
+++ src/bool.erl
@@ -0,0 +1,16 @@
+-module(bool).
+-author("ZnZ").
+
+-export([b_not/1, b_and/2, b_or/2, b_nand/2]).
+
+b_not(true) -> false;
+b_not(false) -> true.
+
+b_and(true, true) -> true;
+b_and(_, _) -> false.
+
+b_or(true, _) -> true;
+b_or(_, true) -> true;
+b_or(_, _) -> false.
+
+b_nand(V1, V2) -> b_not(b_and(V1, V2)).
