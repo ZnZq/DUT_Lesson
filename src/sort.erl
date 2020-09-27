40217323d6e205c06a99a364550cded58f8e1301
ZnZq <gazttgaz@gmail.com>
2020-09-27 20:53:11 +0300

    split

--- src/sort.erl
+++ src/sort.erl
@@ -2,10 +2,31 @@
 -author("ZnZ").
 
 -import(list, [concatenate/1]).
--export([fast/1]).
+-import(arr_r, [len/1]).
+-export([fast/1, merge/1, split/1]).
 
 fast([]) -> [];
 fast([H | T]) ->
-  Left = fast([X || X <- T, X > H]),
-  Right = fast([X || X <- T, X < H]),
-  list:concatenate([Right, [H], Left]).
+  Left = fast([X || X <- T, X < H]),
+  Right = fast([X || X <- T, X > H]),
+  list:concatenate([Left, [H], Right]).
+
+% TODO
+merge([]) -> [];
+merge(List) ->
+  {Left, Right} = split(List),
+  SortedLeft = merge(Left),
+  SortedRight = merge(Right).
+
+split(List) ->
+  Center = round(arr_r:len(List) / 2),
+  Left =  split(List, fun(I) -> I =< Center end, 1),
+  Right = split(List, fun(I) -> I > Center end, 1),
+  {Left, Right}.
+
+split([], _, _) -> [];
+split([H | T], Filter, Index) ->
+  case Filter(Index) of
+    true -> [H | split(T, Filter, Index + 1)];
+    _ -> split(T, Filter, Index + 1)
+  end.
