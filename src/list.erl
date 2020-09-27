e2d1522d9138c2c59226990240c385e70bf09a35
ZnZq <gazttgaz@gmail.com>
2020-09-27 18:14:59 +0300

    list r

--- src/list.erl
+++ src/list.erl
@@ -5,11 +5,20 @@
 
 create(N) -> create(N, []).
 reverse_create(N) -> reverse(create(N)).
-filter(L, N) -> [X || X <- L, X =< N].
+filter(L, N) -> [X || X <- L, X =< N], list_filter(L, fun(X) -> X =< N end).
 reverse(L) -> reverse(L, []).
 
 create(0, Total) -> Total;
 create(N, Total) -> create(N - 1, [N | Total]).
 
 reverse([], R) -> R;
-reverse(L, R) -> [H | T] = L, reverse(T, [H] ++ R).
+reverse(L, R) -> [H | T] = L, reverse(T, [H | R]).
+
+list_filter(List, Filter) -> list_filter(List, Filter, []).
+list_filter([], _, Result) -> Result;
+list_filter(List, Filter, Result) ->
+  [H | T] = List,
+  case Filter(H) of
+    true -> [H | list_filter(T, Filter)];
+    _ -> list_filter(T, Filter, Result)
+  end.
